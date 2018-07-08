df1cdcff6cbe0652d6073abd588ed49b90e0988b
AkioKoneko <sashadetush@gmail.com>
2018-07-08 23:09:19 +0300

    53 процента. пока я опять что-то не сломал прервусь Х)

--- 2.Cycles._Lines.-Lists/2.4.2.py
+++ 2.Cycles._Lines.-Lists/2.4.2.py
@@ -1,7 +1,18 @@
 input = input()
-
 now = ""
+last = input[0]
+count = 0
 
-now = input
+# aaaabbasdddd
 
-print(now)
+for i in input:
+    now = i
+    if(last == now):
+        count += 1
+    elif(last != now):
+        print(last, end="")
+        print(count, end="")
+        count = 1
+    last = now
+print(last, end="")
+print(count, end="")
