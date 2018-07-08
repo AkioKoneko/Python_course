e8aaa8f9e6cb89c21b3cf65e97f8d67c4bbd0ae3
AkioKoneko <sashadetush@gmail.com>
2018-07-08 16:08:43 +0300

    игнор теста ?

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
-a = int (input())
-b = int (input())
-if b != 0:
-    print (a / b)
-else:
-    print ("Division by zero")
-    b = int (input ("Input non-zero value "))
-    print (a / b)
+print(" *")
+print("**")
