3e05ef10e5d3732e2f75eb9fd90edb50f7b4b247
AkioKoneko <sashadetush@gmail.com>
2018-07-20 00:53:05 +0300

    1

--- 3.Functions/3.7.1.py
+++ 3.Functions/3.7.1.py
@@ -8,13 +8,16 @@ for i in range(n):
 outputlst = {}
 for i in range(n):
     for j in 0, 2:
-        outputlst[inputlst[i][j]] = 0,0,0,0,0
+        outputlst[inputlst[i][j]] = [0,0,0,0,0]
+
 
 print(outputlst)
 
 # Подсчет количества игр
 for i in range(n):
     for j in 0, 2:
-        outputlst[inputlst[i][j]] += 1,0,0,0,0
+        outputlst[inputlst[i][j]][0] += 1
 
 print(outputlst)
+
+#нихуя не работает пойду отдохну
