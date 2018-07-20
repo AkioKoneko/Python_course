fd9d827e42befa01a7d99c2b5f5d3b41eaef1fac
AkioKoneko <sashadetush@gmail.com>
2018-07-20 03:02:12 +0300

    Сделяль свой говнокод

--- 3.Functions/3.7.1.py
+++ 3.Functions/3.7.1.py
@@ -8,16 +8,29 @@ for i in range(n):
 outputlst = {}
 for i in range(n):
     for j in 0, 2:
-        outputlst[inputlst[i][j]] = [0,0,0,0,0]
+        outputlst[inputlst[i][j]] = [0, 0, 0, 0, 0]
 
-
-print(outputlst)
-
-# Подсчет количества игр
+# Количество побед
 for i in range(n):
     for j in 0, 2:
         outputlst[inputlst[i][j]][0] += 1
 
-print(outputlst)
+# Победы, поражения, ничьи
+for i in range(n):
+    if inputlst[i][1] > inputlst[i][3]:
+         outputlst[inputlst[i][0]][1] += 1
+         outputlst[inputlst[i][0]][4] += 3
+         outputlst[inputlst[i][2]][3] += 1
+    elif inputlst[i][1] == inputlst[i][3]:
+        outputlst[inputlst[i][0]][2] += 1
+        outputlst[inputlst[i][2]][2] += 1
+        outputlst[inputlst[i][0]][4] += 1
+        outputlst[inputlst[i][2]][4] += 1
+    else:
+        outputlst[inputlst[i][2]][1] += 1
+        outputlst[inputlst[i][2]][4] += 3
+        outputlst[inputlst[i][0]][3] += 1
 
-#нихуя не работает пойду отдохну
+for key in outputlst:
+    print(key, end=':')
+    print(*outputlst[key])
