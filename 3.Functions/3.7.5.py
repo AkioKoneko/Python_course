b638dbf00e327b227ed2fad14a707b1185f891a2
AkioKoneko <sashadetush@gmail.com>
2018-07-21 20:43:15 +0300

    есче + 1

---
+++ 3.Functions/3.7.5.py
@@ -0,0 +1,16 @@
+﻿d = {a:[] for a in range(1, 12)}
+a = ""
+
+with open("input.txt", "r", encoding='utf-8') as input:
+    for line in input:
+        line = line.split()
+        d[int(line[0])] += [int(line[2])]
+
+print(d)
+for key in d:
+    if len(d[key]) != 0:
+        print(key, end=' ')
+        print(sum(d[key]) / len(d[key]))
+    else:
+        print(key, end=' -')
+        print()
